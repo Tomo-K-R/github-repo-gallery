a99aa3e8e29692f72866c469a0e0035576f9c7e0
Tomo-K-R <tomomikojo@hotmail.com>
2022-04-13 00:54:57 +0100

    added a function to fetch the list of repos, and a function to display the info about the each repo and added these two functions to the function that displays the profile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // This div is where the profile information will appear. 
 const overview = document.querySelector(".overview");
 const username = "Tomo-K-R";
+// The unordered list to display the repos list
+const reposList = document.querySelector(".repo-list");
+
 const getProfile = async function(){
     const res = await fetch(`https://api.github.com/users/${username}`);
     const profile = await res.json();
@@ -27,5 +30,25 @@ const displayProfile = function(profile){
     `;
   
   overview.append(userInfo);
+  fetchListOfRepos();
 };
 
+// A function to fetch the list of repos.
+const fetchListOfRepos = async function(){
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated_at&per_page=100`);
+    const data = await res.json();
+    console.log(data);
+    displayRepoInfo(data);
+};
+
+// A function to display info about the repo.
+const displayRepoInfo = function(repos){
+    for (const repo of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        const repoName = document.createElement("h3");
+        repoName.innerHTML = `${repo.name}`;
+        li.append(repoName);
+        reposList.append(li);
+    };
+};
